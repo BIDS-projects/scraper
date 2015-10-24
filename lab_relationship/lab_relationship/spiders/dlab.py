c173c64256b75722055c2baf4fdd1a44b366607b
Don Han <don_han@berkeley.edu>
2015-10-24 12:39:34 -0700

    code organization

--- lab_relationship/lab_relationship/spiders/dlab.py
+++ lab_relationship/lab_relationship/spiders/dlab.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from urlparse import urlparse
-from lab_relationship.items import Links
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.http import Request
+from scrapy.item import BaseItem
+from scrapy.loader import ItemLoader
+from scrapy.exceptions import CloseSpider
 import scrapy
 import csv
 import json
@@ -14,18 +16,15 @@ import json
 
 # REFERNCE: (http://stackoverflow.com/questions/5069416/scraping-data-without-having-to-explicitly-define-each-field-to-be-scraped)
 
-from scrapy.item import BaseItem
-from scrapy.contrib.loader import ItemLoader
-from scrapy.exceptions import CloseSpider
 
-class FlexibleItem(dict, BaseItem):
+class MappingItem(dict, BaseItem):
     pass
 
 class DlabSpider(scrapy.Spider):
     name = "dlab"
     def __init__(self):
         #self.mapping = dict()
-        item = FlexibleItem()
+        item = MappingItem()
         self.loader = ItemLoader(item)
 
     def start_requests(self):
@@ -34,44 +33,62 @@ class DlabSpider(scrapy.Spider):
             with open(filename, 'r') as csv_file:
                 reader = csv.reader(csv_file)
                 header = next(reader)
-                # don-han.com
+                requests = []
+                #### DEBUG ####
+
+                #row = next(reader)
+                #seed_url = row[1].strip()
+                #base_url = urlparse(seed_url).netloc
+                #request = Request(seed_url, callback=self.parse_seed)
+                #request.meta['base_url'] = base_url
+                #requests.append(request)
+                #return requests
+
                 row = next(reader)
                 seed_url = row[1].strip()
                 base_url = urlparse(seed_url).netloc
-                #self.mapping[base_url] = set()
-                #item = Links(base_url=seed_url, on_list=[], off_list=[])
                 request = Request(seed_url, callback=self.parse_seed)
                 request.meta['base_url'] = base_url
-                #requests.append(request)
-                yield request
-                # amplab
-            # yield request
+                requests.append(request)
+                return requests # FOR DEBUGGING PURPOSE
+                #### DEBUG ####
+
                 #for row in reader:
-                    #seed_url = row[1]
-                    #item = Links(url=seed_url, on_list=[], off_list=[])
-                    #request = Request(seed_url, callback=self.parse_seed)
-                    #request.meta['item'] = item
-                    #yield request
+                #    seed_url = row[1].strip()
+                #    base_url = urlparse(seed_url).netloc
+                #    request = Request(seed_url, callback=self.parse_seed)
+                #    request.meta['base_url'] = base_url
+                #    yield request
         except IOError:
             raise CloseSpider("A list of websites are needed")
 
     def parse_seed(self, response):
         base_url = response.meta['base_url']
+        # handle external redirect while still allowing internal redirect
+        if urlparse(response.url).netloc != base_url:
+            return
         external_le = LinkExtractor(deny_domains=base_url)
         external_links = external_le.extract_links(response)
         for external_link in external_links:
             # if the url on the list
-#            self.mapping[base_url].add(external_link.url)
             self.loader.add_value(base_url, external_link.url)
 
         internal_le = LinkExtractor(allow_domains=base_url)
         internal_links = internal_le.extract_links(response)
 
         for internal_link in internal_links:
+            #with open("redirect_issue", "a") as f:
+                #f.write("\tINTERNAL LINK: {}\n".format(internal_link.url))
+            #self.logger.info("@@@@: {}".format(internal_link.url))
             request = Request(internal_link.url, callback=self.parse_seed)
             request.meta['base_url'] = base_url
             yield request
 
+    def get_external_links(self, base_url, response):
+        pass
+
+    def get_internal_links(self, base_url, response):
+        pass
+
     def closed(self, reason):
         self.logger.info("@@@@: {}".format(self.loader.load_item()))
-        return self.loader.load_item()
