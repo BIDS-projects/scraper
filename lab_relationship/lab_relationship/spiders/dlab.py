ea10b0e3e84dbf43a93c002474609fbe9dbe7dc4
Don Han <don_han@berkeley.edu>
2015-11-19 14:03:54 -0800

    edit TODO

--- lab_relationship/lab_relationship/spiders/dlab.py
+++ lab_relationship/lab_relationship/spiders/dlab.py
@@ -14,9 +14,8 @@ import json
 # DYNAMIC ITEM REFERNCE: (http://stackoverflow.com/questions/5069416/scraping-data-without-having-to-explicitly-define-each-field-to-be-scraped)
 # Network Analysis Algorithms: https://networkx.github.io/documentation/latest/reference/algorithms.html
 
-# TODO: LIMIT THE NUMBER OF NODES (TOTAL 56, maybe around 10?)
-# TODO: fix the qb3 bug
-# TODO: Only store the external links that point to nodes already in the list
+# TODO: fix the qb3 bug (if the seed url contains path, it fails)
+# TODO: exit if you are on one path for too long (amplab, jenkins)
 
 class MappingItem(dict, BaseItem):
     pass
@@ -28,41 +27,28 @@ class DlabSpider(scrapy.Spider):
     def __init__(self):
         item = MappingItem()
         self.loader = ItemLoader(item)
+        self.filter_urls = list()
 
     def start_requests(self):
-        filename = "data-science-websites.csv"
+        #filename = "data-science-websites.csv"
+        filename = "debug.csv"
         try:
             with open(filename, 'r') as csv_file:
                 reader = csv.reader(csv_file)
                 header = next(reader)
-                #### DEBUG ####
-                #requests = []
-                #row = next(reader)
-                #seed_url = row[1].strip()
-                #base_url = urlparse(seed_url).netloc
-                #request = Request(seed_url, callback=self.parse_seed)
-                #request.meta['base_url'] = base_url
-                #requests.append(request)
-#
-                #row = next(reader)
-                #seed_url = row[1].strip()
-                #base_url = urlparse(seed_url).netloc
-                #request = Request(seed_url, callback=self.parse_seed)
-                #request.meta['base_url'] = base_url
-                #requests.append(request)
-                #return requests # FOR DEBUGGING PURPOSE
-                #### DEBUG ####
-
                 for row in reader:
                     seed_url = row[1].strip()
                     base_url = urlparse(seed_url).netloc
+                    self.filter_urls.append(base_url)
                     request = Request(seed_url, callback=self.parse_seed)
                     request.meta['base_url'] = base_url
+                    #self.logger.info("'{}' REQUESTED".format(seed_url))
                     yield request
         except IOError:
             raise CloseSpider("A list of websites are needed")
 
     def parse_seed(self, response):
+        #self.logger.info("IN PARSE_SEED FOR {}".format(response.url))
         base_url = response.meta['base_url']
         # handle external redirect while still allowing internal redirect
         if urlparse(response.url).netloc != base_url:
@@ -70,8 +56,8 @@ class DlabSpider(scrapy.Spider):
         external_le = LinkExtractor(deny_domains=base_url)
         external_links = external_le.extract_links(response)
         for external_link in external_links:
-            #TODO: if the url on the list
-            self.loader.add_value(base_url, external_link.url)
+            if urlparse(external_link.url).netloc in self.filter_urls:
+                self.loader.add_value(base_url, external_link.url)
 
         internal_le = LinkExtractor(allow_domains=base_url)
         internal_links = internal_le.extract_links(response)
@@ -79,6 +65,7 @@ class DlabSpider(scrapy.Spider):
         for internal_link in internal_links:
             request = Request(internal_link.url, callback=self.parse_seed)
             request.meta['base_url'] = base_url
+            request.meta['dont_redirect'] = True
             yield request
 
     def get_external_links(self, base_url, response):
@@ -92,4 +79,3 @@ class DlabSpider(scrapy.Spider):
         self.logger.info("@@@@: {}".format(output))
         with open(self.output_filename, 'w') as outfile:
             json.dump(output, outfile, sort_keys=True, indent=4, separators=(',', ': '))
-
