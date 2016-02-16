7493fa9180ab1524cb16b3810097c43719f231bd
Sinho Chewi <chewisinho@berkeley.edu>
2016-02-16 01:08:17 -0800

    Added degrees of separation

--- labs/labs/spiders/weblabsraw.py
+++ labs/labs/spiders/weblabsraw.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 # PIPELINE: get a list of data science institutions and their websites -> crawl through each of the listed organization's internal links and aggregate external links -> save as HTML
-from urlparse import urlparse
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
 from scrapy.http import Request
 from scrapy.item import BaseItem
+from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
-from scrapy.exceptions import CloseSpider
 from scrapy.selector import HtmlXPathSelector
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from urlparse import urlparse
 import csv
 import json
+import scrapy
 
 
 class MappingItem(dict, BaseItem):
@@ -25,7 +25,7 @@ class WebLabsRawSpider(scrapy.Spider):
         self.filter_urls = []
 
     def start_requests(self):
-        """Begin requests for DLab"""
+        """Begin requests for DLab."""
         reader = map(lambda x: x.split(','), seed.strip().split('\n'))
         for _, seed_url in reader:
             seed_url = seed_url.strip()
@@ -33,43 +33,46 @@ class WebLabsRawSpider(scrapy.Spider):
             self.filter_urls.append(base_url)
             request = Request(seed_url, callback=self.parse_seed)
             request.meta['base_url'] = base_url
+            request.meta['deg_sep'] = 0
             yield request
 
     def parse_seed(self, response):
-        """Parse response and yield both HTML and new requests"""
+        """Parse response and yield both HTML and new requests."""
 
         base_url = response.meta['base_url']
+        deg_sep = response.meta['deg_sep']
 
-        # allow internal redirects
+        # Allow internal redirects.
         if urlparse(response.url).netloc == base_url:
             yield self.parse_response(response)
 
-            # add external links to loader?
+            # Add external links to loader?
             for link in self.parse_external_links(base_url, response):
                 self.loader.add_value(base_url, link.url)
 
-            # yield requests only for internal links
+            # Yield requests only for internal links.
             for link in self.parse_internal_links(base_url, response):
                 request = Request(link.url, callback=self.parse_seed)
-                request.meta.update({'base_url':base_url, 'dont_redirect':True})
+                request.meta.update({'base_url': base_url, 'deg_sep': deg_sep + 1,
+                    'dont_redirect': True})
                 yield request
 
     def parse_response(self, response):
-        """Get all data from a response object and return"""
+        """Get all data from a response object and return."""
         data = vars(response).copy()
         data.update({
-            '_body':str(data['_body']),
-            'request':vars(data['request'])
+            '_body': str(data['_body']),
+            'request': vars(data['request'])
         })
         return data
 
     def parse_external_links(self, base_url, response):
-        """Return external links using response"""
-        return filter(lambda link:urlparse(link.url).netloc in self.filter_urls,
+        """Return external links using response."""
+        return filter(lambda link: urlparse(link.url).netloc in self.filter_urls,
             LinkExtractor(deny_domains=base_url).extract_links(response))
 
     def parse_internal_links(self, base_url, response):
-        """Return internal links using response"""
+        """Return internal links using response."""
         return LinkExtractor(allow_domains=base_url).extract_links(response)
 
 
